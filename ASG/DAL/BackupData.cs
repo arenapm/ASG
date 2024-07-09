5c7dc4dd31bd381e3c987a6ee78685699043fdcd
arenapm <arena.martin@hotmail.com>
2024-07-09 19:56:41 -0300

    Backup listo, falta restore

---
+++ ASG/DAL/BackupData.cs
@@ -0,0 +1,53 @@
+﻿using ASG.DAL;
+using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Security.Policy;
+
+namespace LAB_Arena.DAL
+{
+    public class BackupData
+    {
+        public static string BackupDatabase()
+        {
+            string directory = AppDomain.CurrentDomain.BaseDirectory + "App_Data\\";
+            string name = "ASG_" + DateTime.Now.ToFileTime() + ".bak";
+
+            Conexion con = new Conexion();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "BackupDatabase";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Directory", directory);
+            cmd.Parameters.AddWithValue("@Name", name);
+
+            con.ExecNonReader(cmd);
+
+            return directory + name;
+        }
+
+        public static void RestoreDatabase(string file)
+        {
+            SqlConnection conexion = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;Integrated Security=True");
+            string db = AppDomain.CurrentDomain.BaseDirectory + "App_Data\\Database.mdf";
+
+            conexion.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conexion;
+            cmd.CommandText = "USE [master] " +
+                $"ALTER DATABASE [{db}] SET SINGLE_USER WITH ROLLBACK IMMEDIATE " +
+                $"RESTORE DATABASE [{db}] FROM  DISK = N'{file}' WITH  FILE = 1,  NOUNLOAD,  REPLACE,  STATS = 5 " +
+                $"ALTER DATABASE [{db}] SET MULTI_USER";
+            cmd.CommandType = CommandType.Text;
+            cmd.ExecuteNonQuery();
+            conexion.Close();
+            GC.Collect();
+        }
+
+        public static void VerificarDatabase()
+        {
+            
+        }
+    }
+}
---
+++ ASG/Respaldos.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="Respaldos" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Respaldos.aspx.cs" Inherits="ASG.Respaldos" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <main>
+        <section aria-labelledby="aspnetTitle">
+            <div class="container text-center">
+                <asp:Button ID="Respaldar" runat="server" Text="Respaldar" OnClick="Respaldar_Click" /><br />
+                <asp:Button ID="Restaurar" runat="server" Text="Restaurar" OnClick="Restaurar_Click" />
+            </div>
+
+        </section>
+
+    </main>
+
+</asp:Content>
